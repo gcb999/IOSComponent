82464c23a182c0e2906184c07fb4a6577c637961
ganchaobo <ganchaobo@sailvan.com>
2016-11-02 18:38:21 +0800

    集成热修复

--- UIComponent/HotUpdate/JSHotUpdate.h
+++ UIComponent/HotUpdate/JSHotUpdate.h
@@ -8,6 +8,14 @@
 
 #import <Foundation/Foundation.h>
 
+/*
+ 参考文章 ：https://github.com/ios122/ios_assets_hot_update
+ 
+ 1： 先更新
+ 
+ 2： 优先图片网络下载(缓存Bundle)，后面读取本地Bundle
+ 
+ */
 @interface JSHotUpdate : NSObject
 
 -(void)updateCompletion: (void(^)(BOOL isSuccess , NSDictionary *backDic,NSError *error))completion;
